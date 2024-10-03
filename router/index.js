c6af787d770ea9c5bdf0d0b350083dda47504b21
Serhii Safonov <137483700+KaratSergio@users.noreply.github.com>
2024-10-03 12:26:13 +0300

    authMiddleware

--- router/index.js
+++ router/index.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import UserController from '../controllers/user-controller.js';
 import { registrationValidation } from '../validators/userValidator.js';
+import { authMiddleware } from '../middleware/auth-middleware.js';
 
 const router = new Router();
 
@@ -9,6 +10,6 @@ router.post('/login', UserController.login);
 router.post('/logout', UserController.logout);
 router.get('/activate/:link', UserController.activate);
 router.get('/refresh', UserController.refresh);
-router.get('/users', UserController.getUsers);
+router.get('/users', authMiddleware, UserController.getUsers);
 
 export default router;
